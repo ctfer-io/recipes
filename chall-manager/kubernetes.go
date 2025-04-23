cb1c5c4a9bd0ae1dfce4f6cb298fc24d4c4a9d09
Lucas TESSON <lucastesson@protonmail.com>
2025-04-23 22:49:02 +0200

    fix: hook type for kubernetes ExposeType

--- chall-manager/kubernetes.go
+++ chall-manager/kubernetes.go
@@ -10,24 +10,22 @@ import (
 
 // ExposeTypeHook transforms string to Chall-Manager SDK's Kubernetes
 // ExposeType corresponding value.
-var ExposeTypeHook = func() mapstructure.DecodeHookFunc {
-	return func(f reflect.Type, t reflect.Type, data any) (any, error) {
-		if t != reflect.TypeOf(k8s.ExposeType(0)) {
-			return data, nil
-		}
-
-		switch f.Kind() {
-		case reflect.String:
-			str := data.(string)
-			switch str {
-			case "NodePort":
-				return k8s.ExposeNodePort, nil
-			case "Ingress":
-				return k8s.ExposeIngress, nil
-			default:
-				return data, fmt.Errorf("unsupported expose type: %s", str)
-			}
-		}
+var ExposeTypeHook mapstructure.DecodeHookFuncType = func(f reflect.Type, t reflect.Type, data any) (any, error) {
+	if t != reflect.TypeOf(k8s.ExposeType(0)) {
 		return data, nil
 	}
+
+	switch f.Kind() {
+	case reflect.String:
+		str := data.(string)
+		switch str {
+		case "NodePort":
+			return k8s.ExposeNodePort, nil
+		case "Ingress":
+			return k8s.ExposeIngress, nil
+		default:
+			return data, fmt.Errorf("unsupported expose type: %s", str)
+		}
+	}
+	return data, nil
 }
